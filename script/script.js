02c46e294a2140ab51467a554cf457bb235e0a22
yjo0514 <yjo0514@naver.com>
2021-07-15 14:40:06 +0900

    0714_Livart_mainPage

---
+++ script/script.js
@@ -0,0 +1,78 @@
+/*eslint-disable*/
+
+$(function(){
+
+    $("#banner").click(function(){
+        $("#layerBg").css({"display":"block"});
+    });
+
+
+    $(".btnArea").click(function(){
+        $("#layerBg").css({"display":"none"});
+    });
+
+
+
+    $("#popPoint1").click(function(){
+        $("#layerBg1").css({"display":"block"});
+    });
+
+
+    $(".btnArea").click(function(){
+        $("#layerBg1").css({"display":"none"});
+    });
+
+
+     $("#popPoint2").click(function(){
+        $("#layerBg2").css({"display":"block"});
+    });
+
+
+    $(".btnArea").click(function(){
+        $("#layerBg2").css({"display":"none"});
+    });
+
+
+      $("#popPoint3").click(function(){
+        $("#layerBg3").css({"display":"block"});
+    });
+
+
+    $(".btnArea").click(function(){
+        $("#layerBg3").css({"display":"none"});
+    });
+
+
+       $("#popPoint4").click(function(){
+        $("#layerBg4").css({"display":"block"});
+    });
+
+
+    $(".btnArea").click(function(){
+        $("#layerBg4").css({"display":"none"});
+    });
+
+
+      $("#popPoint5").click(function(){
+        $("#layerBg5").css({"display":"block"});
+    });
+
+
+    $(".btnArea").click(function(){
+        $("#layerBg5").css({"display":"none"});
+    });
+
+
+      $("#popPoint6").click(function(){
+        $("#layerBg6").css({"display":"block"});
+    });
+
+
+    $(".btnArea").click(function(){
+        $("#layerBg6").css({"display":"none"});
+    });
+
+
+
+
+});
